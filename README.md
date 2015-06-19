342c326d3a7ee3ed6f39d78e5b8c1cdd4d2df7e0
NCH <n.chamalet@c2is.fr>
2015-06-19 20:05:34 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,54 @@
+#both
+
+## detects in real time user interaction type (Mouse or Touch) and switches linked events
+Because [the user may have touch AND a mouse](http://www.html5rocks.com/en/mobile/touchandmouse/).
+
+* **Authors**: idomusha
+
+## Usage
+
+### Add both.min.js before your closing <body> tag, after jQuery (requires jQuery 1.7 +)
+    <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script type="text/javascript" src="both/both.min.js"></script>
+
+### Define mobile device on load (if you know) [OPTIONAL]
+On load the default behavior is to load mouse events.
+If you use a detecting mobile devices solution (like Mobile Detect or Device.js), you can define the device variable to get touch events as soon as the page is loaded.
+```js
+    if (device.tablet()) {
+        Both.options = {
+            device: 'tablet'
+        };
+    }
+    else if (device.mobile()) {
+        Both.options = {
+            device: 'mobile'
+        };
+    }
+```
+
+### Initialize Both
+```js
+    Both.init();
+```
+
+### Add your events for touch and mouse
+```js
+    Both.store('mouse', $('.your-class'), 'mouseenter', function (e) {
+        alert("Mickey");
+    }
+    Both.store('touch', $('.your-class'), 'touchend', function (e) {
+        alert("Tacchi");
+    }
+```
+
+### Get your events according to interaction type detected or defined on load
+```js
+    Both.start();
+```
+
+#### You can also grab Both using bower:
+```
+    bower install both --save
+```
---
+++ both.js
@@ -0,0 +1,254 @@
+/**
+ * both
+ * detects in real time user interaction type (Mouse or Touch) and switches linked events
+ * @author idomusha / https://github.com/idomusha
+ */
+
+(function (window, $) {
+  var s,
+    Both = {
+
+      defaults: {
+        types: [],    // mouse, touch (touch and pen), key
+        device: '',   // desktop, tablet, mobile
+
+        touch: false,
+        mouse: false,
+        key: false,
+        oHandlersData: [],
+        jHandlersData: {},
+        iViewportWidth: 1024,
+        iInterval: 200,
+        debug: false
+      },
+
+      options: {},
+
+      settings: {},
+
+      init: function () {
+        // merge defaults and options, without modifying defaults explicitly
+        this.settings = $.extend({}, this.defaults, this.options);
+        s = this.settings;
+
+        if (s.debug) log("##################### init()");
+
+        if (s.device == 'mobile' || s.device == "tablet" ){
+          Both.array.add(s.types, "touch");
+        }
+        else{
+          Both.array.add(s.types, "mouse");
+        }
+
+        s.oHandlersData = {
+          mouse: [],
+          touch: [],
+          key: []
+        };
+
+        this.bindUIActions();
+      },
+
+      bindUIActions: function () {
+        if (s.debug) log("##################### bindUIActions()");
+
+        var _movewait;
+        var _return;
+        $(document).on('mousemove', function (e) {
+          if (_return || s.types.indexOf('mouse') > -1) return;
+          if (s.debug) log('>>> mousemove');
+          if (typeof _movewait != 'undefined') {
+            clearTimeout(_movewait);
+          }
+          _movewait = setTimeout(function () {
+
+            if (s.debug) log('>>> movewait');
+            $('html').removeClass('touch').addClass('mouse');
+            s.touch = false;
+            s.mouse = true;
+            Both.array.add(s.types, "mouse");
+            Both.array.remove(s.types, "touch");
+            Both.handleInteractionTypeChange(e);
+          }, s.iInterval);
+        });
+
+        $(document).on('touchstart', function (e) {
+          if (s.types.indexOf('touch') > -1) return;
+          if (s.debug) log('>>> touchstart');
+          $('html').removeClass('mouse').addClass('touch');
+          s.touch = true;
+          s.mouse = false;
+          Both.array.add(s.types, "touch");
+          Both.array.remove(s.types, "mouse");
+          Both.handleInteractionTypeChange(e);
+          _return = true;
+        });
+
+        $(document).on('click', function (e) {
+          _return = false;
+        });
+
+      },
+
+      define: function (o) {
+        if (s.debug) {
+          log("##################### define()");
+        }
+
+        var $returnObject = null;
+        // Undefined item
+        if (typeof o === 'undefined') {
+          return;
+        }
+
+        // Object item
+        else if ((typeof o === 'object') && (o !== null)) {
+          $returnObject = o;
+        }
+
+        // Id or class item
+        else if ((typeof o === 'string') /*&& ((o.charAt(0) == '#') || (o.charAt(0) == '.'))*/) {
+          $returnObject = $(o);
+        }
+
+        return $returnObject;
+      },
+
+      start: function () {
+        Both.handleInteractionTypeChange(true);
+      },
+
+      handleInteractionTypeChange: function (e) {
+        var _text = typeof(e) == "boolean" && e ? 'is setted' : 'has changed'
+        if (s.debug) log('---------------------------------------------------');
+        if (s.debug) log('Interaction type ' + _text + ': ' + s.types.toString());
+        if (s.debug) log('---------------------------------------------------');
+        Both.switch();
+      },
+
+      store: function (context, selector, event, handler) {
+        if (s.debug) log("##################### store()");
+
+        if (s.debug) log("- context", context);
+        if (s.debug) log("- selector", selector);
+        if (s.debug) log("- event", event);
+        if (s.debug) log("- handler", handler);
+
+        s.oHandlersData[context].push({
+          selector: selector,
+          event: event,
+          handler: handler
+        });
+
+        if (s.debug) log("s.oHandlersData", s.oHandlersData);
+
+      },
+
+      switch: function () {
+        if (s.debug) log("##################### switch()");
+        var _oType = {
+          on: s.types.indexOf('mouse') > -1 ? 'mouse' : 'touch',
+          off: s.types.indexOf('touch') > -1 ? 'touch' : 'mouse'
+        };
+        _type = 'mouse' in s.types ? 'mouse' : 'touch';
+        Both.on(_oType.on);
+        Both.off(_oType.off);
+      },
+
+      get: function(type){
+        var _oHandlerData = {
+          selector: s.oHandlersData[type][i]['selector'],
+          event: s.oHandlersData[type][i]['event'],
+          handler: s.oHandlersData[type][i]['handler']
+        }
+        return _oHandlerData;
+      },
+
+      on: function (type) {
+        if (s.debug) log("##################### on()");
+
+        for (var i = 0; i < s.oHandlersData[type].length; i++) {
+          var _oHandlerData = Both.get(type);
+          _oHandlerData.selector.on(_oHandlerData.event, _oHandlerData.handler);
+        }
+      },
+
+      off: function (type) {
+        if (s.debug) log("##################### off()");
+        for (var i = 0; i < s.oHandlersData[type].length; i++) {
+          var _oHandlerData = Both.get(type);
+          _oHandlerData.selector.off(_oHandlerData.event, _oHandlerData.handler);
+        }
+      },
+
+      array: {
+
+        add: function (array, item) {
+          if (s.debug) log("##################### array.add()");
+          array.push(item);
+        },
+
+        remove: function (array, item) {
+          if (s.debug) log("##################### array.remove()");
+          var index = array.indexOf(item);
+          array.splice(index, 1);
+        }
+
+      },
+
+      object: {
+
+        add: function (obj, key, item) {
+          if (s.debug) log("##################### object.add()");
+          if (this.collection[key] != undefined)
+            return undefined;
+          this.collection[key] = item;
+          return ++this.count
+        },
+
+        remove: function (obj, key) {
+          if (s.debug) log("##################### object.remove()");
+          if (this.collection[key] == undefined)
+            return undefined;
+          delete this.collection[key]
+          return --this.count
+        },
+
+        iterate: function (obj) {
+          for (var property in obj) {
+            if (obj.hasOwnProperty(property)) {
+              if (typeof obj[property] == "object") {
+               Both.iterate(obj[property]);
+               }
+               else {
+               console.log(property + "   " + obj[property]);
+               }
+            }
+          }
+        },
+
+        get: function (obj, prop) {
+          if (s.debug) log("##################### object.get()");
+          for (var prop in obj) {
+            if (obj.hasOwnProperty(prop)) {
+              return obj[prop];
+            }
+          }
+        }
+
+      },
+
+      destroy: function () {
+        if (s.debug) log("##################### destroy()");
+        $.removeData(Both.get(0));
+      },
+
+      refresh: function () {
+        if (s.debug) log("##################### refresh()");
+        Both.destroy();
+        Both.init();
+      }
+    }
+  window.Both = Both;
+})
+(window, jQuery);
